8ffeb2b1fc6856f5a10ecdcd8c85c16ae2cebb7d
Michael Mendy <montana@linux.com>
2023-11-09 08:30:58 -0800

    Update script to see if the suspicious packets are on a commonly used port.

--- suspackets.sh
+++ suspackets.sh
@@ -6,9 +6,7 @@ COMMON_PORTS="80,443"
 
 capture_suspicious_packets() {
     echo "Monitoring for suspicious packets on $INTERFACE. Press Ctrl+C to stop."
-    # Run tcpdump and filter out common web service ports
     tcpdump -i "$INTERFACE" "tcp and not port $COMMON_PORTS" -nn -tt | while read -r line; do
-        # Check if the line is not from common service ports
         if ! echo "$line" | grep -q "src port \($COMMON_PORTS\)"; then
             echo "$(date '+%Y-%m-%d %H:%M:%S') - $line" >> "$OUTPUT_FILE"
         fi
